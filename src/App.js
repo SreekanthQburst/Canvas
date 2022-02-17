228d2f8cbe36d7622802c70e5ff04e0298b6a228
Sreekanth V <sreekanth.vinod@qburst.com>
2022-02-17 11:49:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useRef } from "react";
+import { Pannellum, PannellumVideo } from "pannellum-react";
+
+import image_360 from "./assets/landscape.jpg";
+import video_360 from "./assets/mountain.mp4";
+import Canvas from "./components/canvas2";
 
 function App() {
+  const imageRef = useRef();
+  const videoRef = useRef();
+
+  const hotspot = (hotSpotDiv, args) => {
+    hotSpotDiv.classList.add("custom-tooltip");
+    var span = document.createElement("span");
+    span.innerHTML = args.text;
+    hotSpotDiv.appendChild(span);
+    span.style.width = span.scrollWidth - 20 + "px";
+    span.style.marginLeft =
+      -(span.scrollWidth - hotSpotDiv.offsetWidth) / 2 + "px";
+    span.style.marginTop = -span.scrollHeight - 12 + "px";
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas />
+      {/* <Pannellum
+        ref={imageRef}
+        width="100%"
+        height="500px"
+        image={image_360}
+        pitch={30}
+        yaw={180}
+        hfov={110}
+        autoLoad
+        onMouseup={(e) => {
+          console.log(
+            imageRef.current.getViewer().getPitch(),
+            imageRef.current.getViewer().getYaw(),
+            e
+          );
+        }}
+      >
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={0}
+          yaw={180}
+          text="sample text"
+          handleClick={(evt, args) => console.log(args.name)}
+          handleClickArg={{ name: "test" }}
+          tooltip={hotspot}
+          tooltipArg={{ text: "hello there" }}
+          cssClass={"custom-hotspot"}
+        />
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={0}
+          yaw={150}
+          handleClick={(evt, name) => console.log(name)}
+          name="hs1"
+          cssClass={"custom-hotspot"}
+        />
+        <Pannellum.Hotspot
+          type="info"
+          pitch={11}
+          yaw={-167}
+          text="Info Hotspot Text 3"
+        />
+
+        <Pannellum.Hotspot
+          type="info"
+          pitch={31}
+          yaw={-107}
+          text="Info Hotspot Text 4"
+          URL="https://github.com/farminf/pannellum-react"
+        />
+      </Pannellum> */}
+
+      {/******  for video 360 component *******/}
+
+      {/* <PannellumVideo
+        ref={videoRef}
+        video={video_360}
+        loop
+        width="100%"
+        height="600px"
+        pitch={10}
+        yaw={180}
+        hfov={140}
+        minHfov={50}
+        maxHfov={180}
+        controls={true}
+        hotspotDebug={false}
+        onMouseup={(e) => {
+          console.log(
+            e,
+            videoRef.current.getViewer().getPitch(),
+            videoRef.current.getViewer().getYaw()
+          );
+        }}
+        onLoad={() => {
+          console.log("panorama loaded");
+        }}
+        onError={(err) => {
+          console.log("Error", err);
+        }}
+        onErrorcleared={() => {
+          console.log("Error Cleared");
+        }}
+        onMousedown={(evt) => {
+          console.log("Mouse Down", evt);
+        }}
+        onTouchstart={(evt) => {
+          console.log("Touch Start", evt);
+        }}
+        onTouchend={(evt) => {
+          console.log("Touch End", evt);
+        }}
+      >
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={0}
+          yaw={150}
+          handleClick={(evt, args) => console.log(args.name)}
+          handleClickArg={{ name: "test" }}
+        />
+
+        <Pannellum.Hotspot
+          type="info"
+          pitch={31}
+          yaw={-57}
+          text="Info"
+          URL="https://github.com/farminf"
+        />
+      </PannellumVideo> */}
     </div>
   );
 }
---
+++ src/components/canvas2.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect, useRef } from "react";
+// import {
+//   Line,
+//   SteppedLine,
+//   PolyLine,
+//   Circle,
+//   Rectangle,
+// } from "draw-shape-reactjs";
+import img from "../assets/sample_img.jpg";
+import "../App.css";
+
+const ParentContainer = () => {
+  const canvas = useRef();
+  let ctx_array = [];
+  var coordinates_array = [];
+  const [state, setState] = useState({
+    point1: [0, 0],
+    point2: [0, 0],
+    widthRect: 0,
+    heightRect: 0,
+  });
+
+  const [ctx, setctx] = useState([]);
+
+  let new_ctx, rect, canvasX, canvasY, drag;
+
+  useEffect(() => {
+    new_ctx = canvas.current.getContext("2d");
+    rect = {};
+    canvasX = canvas.current.offsetLeft;
+    canvasY = canvas.current.offsetTop;
+    drag = false;
+    canvas.current.addEventListener("mousedown", mouseDown, false);
+    canvas.current.addEventListener("mouseup", mouseUp, false);
+    canvas.current.addEventListener("mousemove", mouseMove, false);
+  }, []);
+
+  //   let canvas = document.getElementById("canvas");
+  const onCompleting = () => {
+    //   TODO:api call
+    // ctx.drawImage(img, 0, 0);
+  };
+  const mouseDown = (e) => {
+    rect.x = e.pageX - canvasX + 0;
+    rect.y = e.pageY - canvasY + 0;
+    rect.w = 0;
+    rect.h = 0;
+    setState({
+      point1: [rect.x, rect.y],
+      point2: [0, 0],
+      widthRect: 0,
+      heightRect: 0,
+    });
+    // ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+    drag = true;
+  };
+  const mouseUp = () => {
+    drag = false;
+    console.log(rect);
+    // coordinates_array.push(rect);
+    allRectangles();
+    // draw();
+  };
+
+  const mouseMove = (e) => {
+    if (drag) {
+      rect.w = e.pageX - canvasX - rect.x;
+      rect.h = e.pageY - canvasY - rect.y;
+      setState({
+        point2: [rect.x + rect.w, rect.y + rect.h],
+        point1: [rect.x, rect.y],
+        widthRect: Math.abs(rect.w),
+        heightRect: Math.abs(rect.h),
+      });
+      new_ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+      draw();
+    }
+  };
+
+  const draw = () => {
+    // ctx.setLineDash([2]);
+    new_ctx.stroke();
+    new_ctx.strokeRect(rect.x, rect.y, rect.w, rect.h);
+  };
+  const allRectangles = () => {
+    console.log(coordinates_array);
+    // coordinates_array.forEach((element, index) => {
+    //   new_ctx.strokeRect(element.x, element.y, element.w, element.h);
+    // });
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <img src={img} alt="" />
+        <canvas id="canvas" className="canvas" ref={canvas}>
+          {" "}
+        </canvas>
+      </div>
+      <button onClick={onCompleting}> On completing</button>{" "}
+      <p>
+        {" "}
+        <strong> (X1, Y1): </strong> ({state.point1[0]}, {state.point1[1]})
+      </p>
+      <p>
+        {" "}
+        <strong> (X2, Y2): </strong> ({state.point2[0]}, {state.point2[1]})
+      </p>
+      <p>
+        {" "}
+        <strong> Size: </strong> ({state.widthRect}X{state.heightRect})
+      </p>
+      <p id="demo"> </p>
+    </div>
+  );
+};
+
+export default ParentContainer;
